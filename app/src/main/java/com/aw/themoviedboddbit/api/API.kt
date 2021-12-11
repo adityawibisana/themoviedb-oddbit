c20f43461965d330c2730e8bcdb8b5eafe23d8ef
Aditya Wibisana <aditya.wibisana@gmail.com>
2021-12-11 21:28:21 +0700

    Show image to the list

---
+++ app/src/main/java/com/aw/themoviedboddbit/api/API.kt
@@ -0,0 +1,5 @@
+package com.aw.themoviedboddbit.api
+
+object API {
+    const val BASE_POSTER_PATH = "https://image.tmdb.org/t/p/w342"
+}
--- app/src/main/java/com/aw/themoviedboddbit/views/adapters/DiscoverMovieAdapter.kt
+++ app/src/main/java/com/aw/themoviedboddbit/views/adapters/DiscoverMovieAdapter.kt
@@ -5,6 +5,13 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.aw.themoviedboddbit.databinding.DiscoveryMovieItemBinding
 import com.aw.themoviedboddbit.models.entity.Movie
+import android.R
+import android.graphics.drawable.Drawable
+import com.aw.themoviedboddbit.api.API
+
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.Target
+
 
 class DiscoverMovieAdapter(private val movies: List<Movie>) :
     RecyclerView.Adapter<DiscoverMovieAdapter.MovieViewHolder>() {
@@ -20,6 +27,16 @@ class DiscoverMovieAdapter(private val movies: List<Movie>) :
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
         val movie = movies[position]
         holder.binding.title.text =  movie.title
+
+        movie.poster_path?.also {
+            Glide
+            .with(holder.itemView.context)
+            .load("${API.BASE_POSTER_PATH}${it}")
+            .centerCrop()
+//            .placeholder(R.drawable.loading_spinner)
+            .into(holder.binding.poster)
+        }
+
     }
 
     override fun getItemCount() = movies.size
