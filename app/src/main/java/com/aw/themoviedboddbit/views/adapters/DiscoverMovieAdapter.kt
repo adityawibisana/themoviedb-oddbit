3f2a9f4da002073c3cb729d9d27a054045ba67cc
Aditya Wibisana <aditya.wibisana@gmail.com>
2021-12-11 22:47:33 +0700

    Filling up vote average, count, popularity and release date

--- app/src/main/java/com/aw/themoviedboddbit/views/adapters/DiscoverMovieAdapter.kt
+++ app/src/main/java/com/aw/themoviedboddbit/views/adapters/DiscoverMovieAdapter.kt
@@ -24,6 +24,11 @@ class DiscoverMovieAdapter(private val movies: List<Movie>) :
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
         val movie = movies[position]
         holder.binding.title.text =  movie.title
+        holder.binding.voteAverage.text = movie.vote_average.toString()
+        holder.binding.popularity.text = movie.popularity.toString()
+        holder.binding.release.text = movie.release_date
+        holder.binding.vote.text = movie.vote_count.toString()
+
         movie.poster_path?.also {
             Glide
             .with(holder.itemView.context)
